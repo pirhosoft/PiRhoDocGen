95c5d5f4092d6962b8c2daa1989d02f522154ae7
Luke Larson <luke.larson@gmail.com>
2020-02-24 13:30:31 -0800

    moving project from private repo of old version

---
+++ Editor/DocumentationCategory.cs
@@ -0,0 +1,212 @@
+﻿using PiRhoSoft.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PiRhoSoft.DocGen.Editor
+{
+	[Serializable]
+	public class DocumentationCategory
+	{
+		[Serializable]
+		public class TemplateSet
+		{
+			[Multiline] public string CategoryFile = "{CategoryName}/{CategoryNiceName}/{CategoryId}\n\n{Types}";
+			[Multiline] public string Type = "{TypeName}/{TypeNiceName}/{TypeId}: {TypeFilename}";
+			[Multiline] public string TypeSeparator = "\n";
+
+			[Multiline] public string TypeFile = "{CategoryName}/{CategoryNiceName}/{CategoryId} {TypeNiceName}/{TypeId}/{TypeFilename}\n\n{TypeNamespace}{TypeName}{Generics} : {Bases}\n\n{Sections}";
+
+			[Multiline] public string Generic = "{Name}";
+			[Multiline] public string GenericOpener = "<";
+			[Multiline] public string GenericCloser = ">";
+			[Multiline] public string GenericSeparator = ", ";
+
+			[Multiline] public string BaseOpener = " : ";
+			[Multiline] public string BaseSeparator = ", ";
+
+			[Multiline] public string Section = "{SectionName}/{SectionNiceName}/{SectionId}\n\n{Members}";
+			[Multiline] public string SectionSeparator = "\n\n";
+
+			[Multiline] public string Constructor = "{Name}({Parameters})";
+			[Multiline] public string Field = "{Type} {Name}/{NiceName}";
+			[Multiline] public string Property = "{Decorators}{Type} {Name}";
+			[Multiline] public string Method = "{Decorators}{Type} {Name}{Generics}({Parameters})";
+			[Multiline] public string MemberSeparator = "\n\n";
+
+			[Multiline] public string Parameter = "{Decorators}{Type} {Name}";
+			[Multiline] public string ParameterSeparator = ", ";
+
+			[Multiline] public string Decorator = "{Name} ";
+			[Multiline] public string DecoratorSeparator = "";
+
+			public string InternalLink = "{TypeName}/{TypeNiceName}/{TypeId}: {TypeFilename}";
+			public string UnknownLink = "{TypeName}/{TypeNiceName}/{TypeId}";
+		}
+
+		[Serializable]
+		public class ExternalNamespace
+		{
+			public string Namespace;
+			public string LinkTemplate = "{TypeName}/{TypeNiceName}/{TypeId}: {TypeFilename}";
+		}
+
+		[Serializable]
+		public class ExternalNamespaceList : SerializedList<ExternalNamespace> { }
+
+		public string Name = "";
+		public string CategoryFilename = "Generated/{CategoryId}.txt";
+		public string TypeFilename = "Generated/{CategoryId}/{TypeId}.txt";
+
+		public bool IncludeInTableOfContents = true;
+
+		[EnumButtons] public DocumentationTypeCategory IncludedTypes = DocumentationTypeCategory.All;
+		[List] public DocumentationNamespaceList IncludedNamespaces = new DocumentationNamespaceList();
+		[List] [Inline] public ExternalNamespaceList ExternalNamespaces = new ExternalNamespaceList();
+		[List] [Inline] public DocumentationSectionList Sections = new DocumentationSectionList();
+		[Frame] public TemplateSet Templates = new TemplateSet();
+
+		public string Id { get; private set; }
+		public string NiceName { get; private set; }
+		public IEnumerable<DocumentationCategory> AllCategories { get; private set; }
+
+		private const string _typesTag = "{Types}";
+		private const string _genericNameTag = "{Name}";
+		private const string _arraySuffix = "[]";
+		private const string _genericOpener = "<";
+		private const string _genericCloser = ">";
+
+		public void Generate(IEnumerable<DocumentationCategory> allCategories, string outputFolder)
+		{
+			Id = DocumentationGenerator.GetId(Name);
+			NiceName = DocumentationGenerator.GetNiceName(Name);
+			AllCategories = allCategories;
+
+			var types = GetTypes();
+			var index = new StringBuilder();
+			var files = new List<string>();
+			var first = true;
+
+			foreach (var type in types)
+			{
+				var typeIndex = type.GenerateIndex(this);
+				var typeFile = type.GenerateFile(this);
+
+				if (!first)
+					index.Append(Templates.TypeSeparator);
+
+				index.Append(typeIndex);
+				first = false;
+
+				DocumentationGenerator.WriteFile(outputFolder, type.Filename, typeFile);
+			}
+
+			var contents = Templates.CategoryFile
+				.Replace(DocumentationGenerator.CategoryNameTag, Name)
+				.Replace(DocumentationGenerator.CategoryNiceNameTag, NiceName)
+				.Replace(DocumentationGenerator.CategoryIdTag, Id)
+				.Replace(_typesTag, index.ToString());
+
+			var filename = CategoryFilename
+				.Replace(DocumentationGenerator.CategoryNameTag, Name)
+				.Replace(DocumentationGenerator.CategoryNiceNameTag, NiceName)
+				.Replace(DocumentationGenerator.CategoryIdTag, Id);
+
+			DocumentationGenerator.WriteFile(outputFolder, filename, contents);
+		}
+
+		public IEnumerable<DocumentationType> GetTypes()
+		{
+			return DocumentationGenerator
+				.FindTypes(IsTypeIncluded)
+				.Select(type => new DocumentationType(type, this))
+				.OrderBy(type => type.Name);
+		}
+
+		private bool IsTypeIncluded(Type type)
+		{
+			return DocumentationGenerator.IsTypeIncluded(type, IncludedTypes, IncludedNamespaces);
+		}
+
+		public string GetLink(Type type)
+		{
+			if (type.IsArray)
+			{
+				return GetLink(type.GetElementType()) + _arraySuffix;
+			}
+			else if (type.IsGenericParameter)
+			{
+				return type.Name;
+			}
+			else
+			{
+				var link = GetTypeLink(type);
+				
+				if (type.IsGenericType)
+					link += GetGenerics(type);
+
+				return link;
+			}
+		}
+
+		private string GetTypeLink(Type type)
+		{
+			if (DocumentationGenerator.IsTypeIncluded(type, IncludedNamespaces))
+				return GetTypeLink(type, this, Templates.InternalLink);
+
+			foreach (var external in ExternalNamespaces)
+			{
+				if (DocumentationGenerator.IsTypeIncluded(type, new List<string> { external.Namespace }))
+					return GetTypeLink(type, this, external.LinkTemplate);
+			}
+
+			return GetTypeLink(type, null, Templates.UnknownLink);
+		}
+
+		private string GetTypeLink(Type type, DocumentationCategory category, string template)
+		{
+			var cleanName = DocumentationGenerator.GetCleanName(type);
+			var niceName = DocumentationGenerator.GetNiceName(cleanName);
+			var id = DocumentationGenerator.GetTypeId(type);
+
+			return template
+				.Replace(DocumentationGenerator.CategoryNameTag, category?.Name)
+				.Replace(DocumentationGenerator.CategoryNiceNameTag, category?.NiceName)
+				.Replace(DocumentationGenerator.CategoryIdTag, category?.Id)
+				.Replace(DocumentationGenerator.TypeIdTag, id)
+				.Replace(DocumentationGenerator.TypeNameTag, cleanName)
+				.Replace(DocumentationGenerator.TypeRawNameTag, type.Name)
+				.Replace(DocumentationGenerator.TypeNiceNameTag, niceName)
+				.Replace(DocumentationGenerator.TypeNamespaceTag, type.Namespace)
+				.Replace('`', '-'); // this is specific to msdn but potentially makes sense for other scenarios as well
+		}
+
+		public string GetGenerics(Type type)
+		{
+			var builder = new StringBuilder();
+
+			var generics = type.IsConstructedGenericType
+				? type.GetGenericArguments()
+				: type.GetGenericTypeDefinition().GetGenericArguments();
+
+			if (generics.Length > 0)
+			{
+				builder.Append(_genericOpener);
+
+				for (var i = 0; i < generics.Length; i++)
+				{
+					if (i != 0)
+						builder.Append(Templates.GenericSeparator);
+
+					var generic = type.IsConstructedGenericType ? GetLink(generics[0]) : Templates.Generic.Replace(_genericNameTag, generics[0].Name);
+					builder.Append(generic);
+				}
+
+				builder.Append(_genericCloser);
+			}
+
+			return builder.ToString();
+		}
+	}
+}
---
+++ Editor/DocumentationGeneratorWindow.cs
@@ -0,0 +1,195 @@
+﻿using PiRhoSoft.Utilities.Editor;
+using System;
+using System.IO;
+using System.Threading;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace PiRhoSoft.DocGen.Editor
+{
+	public class DocumentationGeneratorWindow : EditorWindow
+	{
+		private DocumentationGenerator _generator;
+		private GenerationState _state = GenerationState.Waiting;
+		private string _message = string.Empty;
+		private float _progress = 0.0f;
+		private Thread _thread = null;
+		private string _applicationPath;
+
+		[MenuItem("Window/PiRho Soft/Documentation Generator")]
+		public static void Open()
+		{
+			GetWindow<DocumentationGeneratorWindow>("Documentation Generator").Show();
+		}
+
+		void OnEnable()
+		{
+			_applicationPath = Application.dataPath;
+			_state = GenerationState.Waiting;
+
+			LoadGenerator();
+			CreateElements();
+		}
+
+		void OnDisable()
+		{
+			SaveGenerator();
+			UnloadGenerator();
+		}
+
+		void OnInspectorUpdate()
+		{
+			if (_state != GenerationState.Waiting)
+				Repaint();
+		}
+
+		void CreateElements()
+		{
+			var serializedObject = new SerializedObject(_generator);
+
+			var generateButton = new Button(StartGeneration) { text = "Generate" };
+			var outputDirectoryProperty = serializedObject.FindProperty(nameof(DocumentationGenerator.OutputDirectory));
+			var categoriesProperty = serializedObject.FindProperty(nameof(DocumentationGenerator.Categories));
+			var tableOfContentsProperty = serializedObject.FindProperty(nameof(DocumentationGenerator.TableOfContents));
+			var logDescriptionsProperty = serializedObject.FindProperty(nameof(DocumentationGenerator.LogDescriptions));
+			var helpUrlsProperty = serializedObject.FindProperty(nameof(DocumentationGenerator.HelpUrls));
+
+			var scrollContainer = new ScrollView(ScrollViewMode.Vertical);
+
+			scrollContainer.Add(generateButton);
+			scrollContainer.Add(new PropertyField(outputDirectoryProperty));
+			scrollContainer.Add(new PropertyField(categoriesProperty));
+			scrollContainer.Add(new PropertyField(tableOfContentsProperty));
+			scrollContainer.Add(new PropertyField(logDescriptionsProperty));
+			scrollContainer.Add(new PropertyField(helpUrlsProperty));
+
+			rootVisualElement.Add(scrollContainer);
+			rootVisualElement.Bind(serializedObject);
+		}
+
+		void OnGUI()
+		{
+			if (_state == GenerationState.Done || _state == GenerationState.Error)
+			{
+				FinishGeneration();
+				EditorUtility.ClearProgressBar();
+			}
+			else if (_state != GenerationState.Waiting)
+			{
+				EditorUtility.DisplayProgressBar("Generating Documentation", _message, _progress);
+			}
+		}
+
+		#region Generation
+
+		private enum GenerationState
+		{
+			Waiting,
+			Starting,
+			Categories,
+			TableOfContents,
+			Log,
+			Help,
+			Done,
+			Error
+		}
+
+		private void SetProgress(GenerationState state, float progress, string message)
+		{
+			_state = state;
+			_progress = progress;
+			_message = message;
+		}
+
+		private void StartGeneration()
+		{
+			if (_thread == null)
+			{
+				SetProgress(GenerationState.Starting, 0.0f, "Setting up generator");
+				_thread = new Thread(GenerateAllThread);
+				_thread.Start();
+			}
+		}
+
+		private void FinishGeneration()
+		{
+			_thread.Join();
+			_state = GenerationState.Waiting;
+			_thread = null;
+		}
+
+		private void GenerateAllThread()
+		{
+			try
+			{
+				var steps = _generator.Categories.Count + 3.0f;
+				var progress = 1.0f;
+
+				foreach (var category in _generator.Categories)
+				{
+					SetProgress(GenerationState.Categories, progress / steps, string.Format("Generating {0} category", category.Name));
+					category.Generate(_generator.Categories, _generator.OutputDirectory);
+					progress += 1.0f;
+				}
+
+				SetProgress(GenerationState.TableOfContents, progress / steps, "Generating table of contents");
+				_generator.TableOfContents.Generate(_applicationPath, _generator.Categories, _generator.OutputDirectory);
+				progress += 1.0f;
+
+				SetProgress(GenerationState.Log, progress / steps, "Generating log descriptions");
+				_generator.LogDescriptions.Generate(_generator.OutputDirectory);
+				progress += 1.0f;
+
+				SetProgress(GenerationState.Help, progress / steps, "Validating help urls");
+				_generator.HelpUrls.Validate();
+				progress += 1.0f;
+
+				SetProgress(GenerationState.Done, 1.0f, "Generation complete");
+			}
+			catch (Exception exception)
+			{
+				Debug.LogException(exception);
+				SetProgress(GenerationState.Error, 1.0f, "Generation error");
+			}
+		}
+
+		#endregion
+
+		#region File IO
+
+		private void LoadGenerator()
+		{
+			var path = GetSettingsPath();
+			var content = File.ReadAllText(path);
+
+			_generator = CreateInstance<DocumentationGenerator>();
+
+			JsonUtility.FromJsonOverwrite(content, _generator);
+		}
+
+		private void UnloadGenerator()
+		{
+			DestroyImmediate(_generator);
+			_generator = null;
+		}
+
+		private void SaveGenerator()
+		{
+			var path = GetSettingsPath();
+			var content = JsonUtility.ToJson(_generator, true);
+			var outputFile = new FileInfo(path);
+
+			Directory.CreateDirectory(outputFile.Directory.FullName);
+			File.WriteAllText(outputFile.FullName, content);
+		}
+
+		private string GetSettingsPath()
+		{
+			return AssetHelper.GetScriptPath() + "Settings.json";
+		}
+
+		#endregion
+	}
+}
